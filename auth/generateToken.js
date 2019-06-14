a06c9eaf41587537a1e354d9a5798338b5642791
Jonathan Taylor <jonyonson@icloud.com>
2019-06-14 11:20:44 -0500

    MVP ✅

---
+++ auth/generateToken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const secret = require('../config/secrets').jwtSecret;
+
+module.exports = user => {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secret, options);
+};
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const authDb = require('../database/auth-model.js');
 
 const { authenticate } = require('../auth/authenticate');
+const generateToken = require('../auth/generateToken.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,11 +12,43 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  authDb
+    .add(user)
+    .then(saved => {
+      const token = generateToken(saved);
+      // console.log(token);
+      res.status(201).json({ saved, token });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  authDb
+    .findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
